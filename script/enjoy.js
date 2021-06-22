e961732d9934bd59e5064afc3bef7b4632f254f8
lazypaint2 <126423@naver.com>
2021-06-23 02:13:33 +0900

    0623 여기서 브랜치 다시 분기합니다-------------------------

---
+++ script/enjoy.js
@@ -0,0 +1,13 @@
+
+var swiper2 = new Swiper(".enjoySwiper2", {
+    // mousewheel: true,
+    loop: true,
+    // autoplay: {
+    //   delay: 2500,
+    //   disableOnInteraction: false,
+    // },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
