8ff101a4798d09003371789db872e8778e404b85
lazypaint2 <126423@naver.com>
2021-06-24 18:08:27 +0900

    0624 인트로애니, 인트로 이미지폴더, 파비콘 추가

--- script/script.js
+++ script/script.js
@@ -2,7 +2,6 @@ $(function(){
     
     $('#box').empty()
    
-   
     //#wrap안에 #main로드, 섹션안에 home.html로드
    $('#intro a').click(function(e){
        e.preventDefault()
@@ -22,5 +21,42 @@ $(function(){
        var href=$(this).attr('href')
        $('#content').load(href)
    })
+
+   //intro
+   window.onload = function() { 
+     setTimeout(function(){
+         $('.girl, .guitar').delay(3000).fadeIn(800, function(){ 
+            $('.boy').fadeIn(500, function(){
+                $('.bear').fadeIn(500, function(){
+                    $('.chair').fadeIn(450, function(){
+                        $('.record').fadeIn(450, function(){
+                            $('.fire').fadeIn(300, function(){
+                                $('.radial1').fadeIn(200, function(){
+                                    $('.smore').fadeIn(200, function(){
+                                        $('.sausage').fadeIn(200, function(){
+                                            $('.bearBox').fadeIn(300, function(){
+                                                $('.light').fadeIn(300, function(){
+                                                    $('.radial2').fadeIn(100, function(){
+                                                        $('.dog').fadeIn(300, function(){
+                                                            $('.btnHome').delay(500).fadeIn(3000)
+                                                        })
+                                                    })
+                                                })
+                                            })                        
+                                        })
+
+                                    })
+        
+                                })
+    
+                            })
+                        })                    
+                    })
+
+                })
+            })
+         })
+     })
+    }
    
 })
