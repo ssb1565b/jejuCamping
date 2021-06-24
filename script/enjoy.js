4c2071a4e2ffc6552c56aa48b184b4da567d1ca7
naxona <6owtolove@gmail.com>
2021-06-24 19:18:59 +0900

    0624 hj

--- script/enjoy.js
+++ script/enjoy.js
@@ -1,11 +1,41 @@
+var ww = $(window).width();
+
+        function initSwiper() {
+          mySwiper = new Swiper(".platlist_slide", {
+            slidesPerView: 1,
+            spaceBetween: 10,
+            simulateTouch: true,
+            loop: true,
+          });
+        }
+
+        if (ww < 1263) {
+          initSwiper();
+        } else if (ww > 1262) {
+
+        }
+        $(window).on('resize', function () {
+          ww = $(window).width();
+          if (ww < 1263) {
+            initSwiper();
+            console.log(ww)
+          }else if(ww > 1264 && ww < 1300){
+            document.location.reload(1000)
+          }else{
+
+          }
+        });
+
+
+
 
 var swiper2 = new Swiper(".enjoySwiper2", {
     // mousewheel: true,
     loop: true,
-    // autoplay: {
-    //   delay: 2500,
-    //   disableOnInteraction: false,
-    // },
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
